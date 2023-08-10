b3d760cd5ae9ac8ad6782f606a35e3be67f0d24a
EG-AKANDE <emmanuel_akande@yahoo.com@yahoo.com>
2023-08-10 16:35:11 -0700

    user.py

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''class user from BaseModel'''
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+        '''rep class user'''
+
+            email = ""
+                password = ""
+                    first_name = ""
+                        last_name = ""
